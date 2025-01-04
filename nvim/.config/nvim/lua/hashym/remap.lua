32d482266582bc3796d5697a50c5af5add45e7db
Hasse Vestergaard Hymøller <hassevgaard@gmail.com>
2025-01-04 23:15:22 +0100

    Add integration between tmux and nvim

--- nvim/.config/nvim/lua/hashym/remap.lua
+++ nvim/.config/nvim/lua/hashym/remap.lua
@@ -4,6 +4,11 @@ vim.g.mapleader = ' '
 -- Open Netwr Explorer
 vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
 
+vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
+vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
+vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
+vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+
 -- Move multiple lines with correct indentation
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
